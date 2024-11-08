9e007699e5dca1e560fe016eb4d43b5070c71762
buzznutbrainz <ava.ehtesham-heravi@edu.sait.ca>
2024-11-07 18:01:35 -0700

    images

--- lab2.js
+++ lab2.js
@@ -1,4 +1,21 @@
-bear.addEventListener('clicl'function(){
-document.getElementById("bear").src="bear.webp";
+const image = document.getElementById("animal");
 
-})
+document.getElementById("sheep").addEventListener("click", function() {
+    image.src = "sheep.webp";
+});
+
+document.getElementById("bear").addEventListener("click", function() {
+    image.src = "bear.webp";
+});
+
+document.getElementById("moose").addEventListener("click", function() {
+    image.src = "moose.webp";
+});
+
+document.getElementById("wolf").addEventListener("click", function() {
+    image.src = "wolf.webp";
+});
+
+document.getElementById("elk").addEventListener("click", function() {
+    image.src = "elk.webp";
+});
